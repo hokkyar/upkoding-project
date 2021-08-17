a453d0bdd4def490e35142c4ceea608dbec6835c
Hokky Aryasta <81088460+hokkyar@users.noreply.github.com>
2021-08-18 07:42:19 +0800

    adding main method

--- Sorting.java
+++ Sorting.java
@@ -1,4 +1,8 @@
 public class Sorting {
+    public static void main(String[] args) {
+        int[] nums = {2,7,4,9,10,1};
+        bubbleSort(nums);
+    }
     private static void bubbleSort(int[] nums){
         // using bubble sort and make it to ascending order (from lowest to highest)
         for(int i = 0; i < nums.length; i++){
