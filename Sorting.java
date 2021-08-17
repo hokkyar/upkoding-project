2b6af95b64a70e90fdcc879b5c9bc795ae884ac9
Hokky Aryasta <81088460+hokkyar@users.noreply.github.com>
2021-08-18 07:31:58 +0800

    upload a file

---
+++ Sorting.java
@@ -0,0 +1,24 @@
+package SortingArray;
+
+public class Sorting {
+    public static void main(String[] args) {
+        int[] nums = {2,7,4,9,10,1};
+        bubbleSort(nums);
+    }
+    private static void bubbleSort(int[] nums){
+        // using bubble sort and make it to ascending order (from lowest to highest)
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] > nums[j]){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        // display the result
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+}
